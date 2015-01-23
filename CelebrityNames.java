dec11b1e7aff421c79772a3dfbd23f40c705e2f6
patrickjn <nicholas.patrick@mrjaffesclass.com>
2015-01-22 22:26:16 -0800

    Fixed Changes

--- CelebrityNames.java
+++ CelebrityNames.java
@@ -30,10 +30,7 @@ public class CelebrityNames
         { 
             Scanner sc = new Scanner( text[j] );
             String firstName = sc.next( );
-            String middleName = sc.next( );
             String lastName = sc.next( );
-            String birthDate = sc.next( );
-            reversedName[j] = lastName + ", " + firstName + " " + middleName + " -- " + birthDate;
         } 
         
         Arrays.sort(reversedName);
